f356dcfe4fe9ed53333b4bb2a4f12cfe5f5e3c62
Nick Spencer <strangebrew202@gmail.com>
2023-11-06 09:43:07 -0700

    Snapshot: result poro and result serializer incomplete, pushing so Kaylee can fetch back down and work on this branch

--- app/poros/result.rb
+++ app/poros/result.rb
@@ -1,12 +1,20 @@
 class Result
-  attr_reader :id, :total_wins, :total_loses, :total_rounds, :win_percentage
+  attr_reader :id, :total_wins, :total_loses, :total_rounds, 
+              :win_percentage, :p1_stats, :p2_stats, :p3_stats,
+              :p4_stats, :p5_stats
 
   def initialize(sim_id)
     @id = sim_id
+    @sim = Simulation.find(@id)
     @total_rounds = 
     @total_wins = total_wins
     @total_loses = total_loses
     @win_percentage = win_percentage
+    @p1_stats = stats("p1")
+    @p2_stats = stats("p2")
+    @p3_stats = stats("p3")
+    @p4_stats = stats("p4")
+    @p5_stats = stats("p5")
   end
 
   def total_rounds
@@ -38,5 +46,22 @@ class Result
     .select("combats.*, sum(combat_rounds.#{creature}_damage_dealt) as total_damage")
     .group(:id)
     .order(:id)
+  end
+
+  def hit_rate(p)
+    Simulation.find(@id)
+    .combat_results
+    .average("#{p}_attacks_successful::float / #{p}_attacks_attempted::float")
+  end
+
+  def avg_damage_per_round(p)
+    (@sim.combat_rounds.sum("#{p}_damage_dealt").to_f / @sim.combat_rounds.count.to_f).round(2)
+  end
+
+  def stats(p)
+    {
+      player: p,
+      avg_dmg: avg_damage_per_round(p)
+    }
   end
 end
--- app/serializers/result_serializer.rb
+++ app/serializers/result_serializer.rb
@@ -1,4 +1,4 @@
 class ResultSerializer
 include JSONAPI::Serializer
-attributes :id, :total_wins, :total_loses, :total_rounds, :win_percentage
+attributes :id, :total_wins, :total_loses, :total_rounds, :win_percentage, :p1_stats, :p2_stats, :p3_stats, :p4_stats, :p5_stats
 end
